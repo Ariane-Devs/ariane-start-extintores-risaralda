0b146d0826995331f56dc2c78168d50008eccf38
Sebastian Ramirez <sebastian@jsramirez.com>
2025-07-24 07:35:33 -0500

    feat: update contact information and improve address display in ContactSection and index page

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,12 +1,12 @@
 import ogImageSrc from "@images/social.png";
 
-
-
 export const SITE = {
   title: "Extintoresdelrisaralda",
   tagline: "Top-quality Hardware Tools",
-  description: "Extintoresdelrisaralda offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "Extintoresdelrisaralda offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  description:
+    "Extintoresdelrisaralda es tu aliado en seguridad contra incendios: venta de extintores, gabinetes, señalización y servicios de instalación y mantenimiento profesional. Protege tu empresa, hogar o proyecto con productos certificados y atención experta en Risaralda y el Eje Cafetero.",
+  description_short:
+    "Venta de extintores, gabinetes, señalización y servicios de instalación y mantenimiento profesional en Risaralda.",
   url: "https://screwfast.uk",
   author: "Emil ArianeDevs",
 };
@@ -35,30 +35,31 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with Extintoresdelrisaralda's top-quality hardware tools and expert construction services. Trusted by industry leaders, Extintoresdelrisaralda offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  title: `${SITE.title}: Extintores del Risaralda`,
+  description:
+    "Equip your projects with Extintoresdelrisaralda's top-quality hardware tools and expert construction services. Trusted by industry leaders, Extintoresdelrisaralda offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
   image: ogImageSrc,
 };
 
 export const partnersData = [
-    {
-        icon: '<img src="/partners/partner1.png" alt="Partner 1" class="mx-auto h-auto w-40 py-3 sm:mx-0 lg:w-70 lg:py-5" />',
-        name: "First",
-        href: "#",
-    },
-    {
-        icon: '<img src="/partners/partner2.png" alt="Partner 2" class="mx-auto h-auto w-36 py-3 sm:mx-0 lg:w-50 lg:py-5" />',
-        name: "Second",
-        href: "#",
-    },
-    {
-        icon: '<img src="/partners/partner3.png" alt="Partner 3" class="mx-auto h-auto w-30 py-3 sm:mx-0 lg:w-40 lg:py-5" />',
-        name: "Third",
-        href: "#",
-    },
-    {
-        icon: '<img src="/partners/partner4.png" alt="Partner 4" class="mx-auto h-auto w-32 py-3 sm:mx-0 lg:w-50 lg:py-5" />',
-        name: "Fourth",
-        href: "#",
-    },
-]
+  {
+    icon: '<img src="/partners/partner1.png" alt="Partner 1" class="mx-auto h-auto w-40 py-3 sm:mx-0 lg:w-70 lg:py-5" />',
+    name: "First",
+    href: "#",
+  },
+  {
+    icon: '<img src="/partners/partner2.png" alt="Partner 2" class="mx-auto h-auto w-36 py-3 sm:mx-0 lg:w-50 lg:py-5" />',
+    name: "Second",
+    href: "#",
+  },
+  {
+    icon: '<img src="/partners/partner3.png" alt="Partner 3" class="mx-auto h-auto w-30 py-3 sm:mx-0 lg:w-40 lg:py-5" />',
+    name: "Third",
+    href: "#",
+  },
+  {
+    icon: '<img src="/partners/partner4.png" alt="Partner 4" class="mx-auto h-auto w-32 py-3 sm:mx-0 lg:w-50 lg:py-5" />',
+    name: "Fourth",
+    href: "#",
+  },
+];
